a4931dff4b8ba851125963ccda455e5787c2efa3
dsinitsyn <dima@pindrop.nl>
2017-12-20 16:23:08 +0200

    start script added

--- server.js
+++ server.js
@@ -2,20 +2,23 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
+
+
 let app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 
-app.use((req, res, next) =>{ //Guard?
+app.use((req, res, next) => { //Guard?
     let now = new Date().toString();
     let log = `${now}: ${req.method} ${req.url}`;
     console.log(log);
-    fs.appendFile('server.log', log + '\n', (err) =>{
+    fs.appendFile('server.log', log + '\n', (err) => {
         err && console.log('error');
 
     });
-    //next();
+    next();
 });
 
 //app.use((req, res, next) =>{
@@ -24,34 +27,34 @@ app.use((req, res, next) =>{ //Guard?
 
 app.use(express.static(__dirname + '/public')); //public folder
 
-
-hbs.registerHelper('getYear', () =>{
+hbs.registerHelper('getYear', () => {
     return new Date().getFullYear();
 });
 
-hbs.registerHelper('screamIt', (text) =>{
+hbs.registerHelper('screamIt', (text) => {
     return text.toUpperCase();
 });
 
-app.get('/', (req, res) =>{
+app.get('/', (req, res) => {
     res.render('home.hbs', {
         title: 'Home page',
         welcomeMessage: 'Welcome'
     })
 });
 
-app.get('/about', (req, res) =>{
+app.get('/about', (req, res) => {
     res.render('about.hbs', {
         title: 'About page'
     })
 });
 
-app.get('/bad', (req, res) =>{
+app.get('/bad', (req, res) => {
     res.send({
         errorMessage: 'Unable to handle request'
     });
 });
 
-app.listen(3000, () =>{
-    console.log('Server is up on port 3000');
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
+
 });
